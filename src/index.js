6a59993b86a9d14189e63be47ba125f938e4bf7f
Patrick Delaney <patrickdelaney@Patricks-MacBook-Pro-2.local>
2026-03-25 12:19:25 +1100

    Add quickcompare and config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-Import QuickCompare from './QuickCompare';
+import QuickCompare from './QuickCompare';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
